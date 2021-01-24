46d6463173a6dfc71b36eccdb3f60b3f3163647b
blood <hey-blood@foxmail.com>
2021-01-24 18:06:44 +0800

    练习：修复应用

--- app/src/main/java/com/example/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -37,11 +38,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
 
-        // Update the information displayed to the user.
-        updateUi(earthquake);
     }
 
     /**
@@ -56,6 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... urls) {
+            // Perform the HTTP request for earthquake data and process the response.
+            Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+            return earthquake;
+        }
+
+        @Override
+        protected void onPostExecute(Event result) {
+            // Update the information displayed to the user.
+            updateUi(result);
+        }
     }
 
 }
